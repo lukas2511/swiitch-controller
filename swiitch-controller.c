f11b942533e9d8fd3d972df3d3b8d674bc5d8a94
Lukas Schauer <lukas@schauer.so>
2017-11-23 11:23:06 +0100

    cleanup

--- swiitch-controller.c
+++ swiitch-controller.c
@@ -1,35 +1,16 @@
 #include <stdlib.h>
-#include <string.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/usb/usbd.h>
-#include <libopencm3/usb/hid.h>
 
 #include "controller.h"
 #include "usb.h"
 
+union Controller controller;
+
 int main(void)
 {
   rcc_clock_setup_in_hsi_out_48mhz();
-
-  rcc_periph_clock_enable(RCC_GPIOA);
-  /*
-   * This is a somewhat common cheap hack to trigger device re-enumeration
-   * on startup.  Assuming a fixed external pullup on D+, (For USB-FS)
-   * setting the pin to output, and driving it explicitly low effectively
-   * "removes" the pullup.  The subsequent USB init will "take over" the
-   * pin, and it will appear as a proper pullup to the host.
-   * The magic delay is somewhat arbitrary, no guarantees on USBIF
-   * compliance here, but "it works" in most places.
-   */
-  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-      GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
-  gpio_clear(GPIOA, GPIO12);
-  for (unsigned i = 0; i < 800000; i++) {
-    __asm__("nop");
-  }
 
   systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
   /* SysTick interrupt every N clock pulses: set reload to N-1 */
@@ -39,8 +20,6 @@ int main(void)
 
   init_usb();
 }
-
-union Controller controller;
 
 void sys_tick_handler(void)
 {
