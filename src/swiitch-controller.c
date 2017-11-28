312deb96f17807f9352afc151c7dc88a280e4614
Lukas Schauer <lukas@schauer.so>
2017-11-28 19:34:46 +0100

    use external clock and set pll to 72mhz

--- src/swiitch-controller.c
+++ src/swiitch-controller.c
@@ -56,7 +56,7 @@ static fibre_t debug_loop_task = FIBRE_VAR_INIT(debug_loop);
 
 
 int main(void) {
-  rcc_clock_setup_in_hsi_out_48mhz();
+  rcc_clock_setup_in_hse_8mhz_out_72mhz();
   rcc_periph_clock_enable(RCC_GPIOC);
   gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
